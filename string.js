df48040b7117f2b55d46abcf2eec2938bf41b8a2
atikur-7 <asraful0508@gmail.com>
2024-02-28 01:02:28 +0600

    solved some js sting related problems

--- string.js
+++ string.js
@@ -1,4 +1,4 @@
-/* ------------ Find the index number of the largest string in the list of members and return reverse output ------------ */
+/* Problem: Find the index number of the largest string in the list of members and return reverse output */
 const memberOne = "Abdullah Al Masud";
 const memberTwo = "Nafees Imtiaz Shawon";
 const memberThree = "Sihab Uddin Molla";
@@ -23,7 +23,7 @@ const findLargestString = (memberOne, memberTwo, memberThree) => {
 const largestStr = findLargestString(memberOne, memberTwo, memberThree);
 // console.log(largestStr);
 
-/* ----------- Given a string, the task is to reverse the order of the words in the given string ----------- */
+/* Problem: Given a string, the task is to reverse the order of the words in the given string */
 function reverseWords(inputString) {
     let reversedString;
     if (typeof inputString === "string" && inputString.length > 0) {
@@ -34,10 +34,9 @@ function reverseWords(inputString) {
     return reversedString;
 }
 const input = "i like this program very much";
-const output = reverseWords(input);
-// console.log(output);
+// console.log(reverseWords(input));
 
-/* ----------- Given a set of strings, find the longest common prefix ----------- */
+/* Problem: Given a set of strings, find the longest common prefix */
 function longestCommonPrefix(arr) {
     if (arr.length === 0) return "";
 
@@ -64,6 +63,7 @@ function longestCommonPrefix(arr) {
 const inputArr = ["geeksforgeeks", "geeks", "geek", "geezer"];
 // console.log("The longest Common Prefix is:", longestCommonPrefix(inputArr));
 
+/* Problem: Given two strings of lowercase alphabets and a value k, the task is to find if two strings are K-anagrams of each other or not */
 function areKAnagrams(s1, s2, k) {
     if (s1.length !== s2.length) {
         return false;
@@ -97,8 +97,41 @@ function areKAnagrams(s1, s2, k) {
 const str1 = "anagram";
 const str2 = "grammar";
 const k = 2;
-if (areKAnagrams(str1, str2, k)) {
-    console.log("Yes");
-} else {
-    console.log("No");
+// if (areKAnagrams(str1, str2, k)) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+/* Problem: Create a function that counts the number of occurrences of a specific substring within a given string. Utilize the relevant string method to simplify the implementation. */
+function countOccurrences(mainString, subString) {
+    const regex = new RegExp(subString, "gi");
+    const matches = mainString.match(regex);
+    return matches ? matches.length : 0;
 }
+// console.log(countOccurrences("hello hello Hello world", "hello"));
+
+/* Problem: Implement a function that capitalizes the first letter of each word in a sentence. */
+function capitalizeWords(sentence) {
+    const splitWords = sentence.split(" ");
+    const uppercaseFirstLetter = splitWords.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
+    const capitalizeSentence = uppercaseFirstLetter.join(" ");
+    return capitalizeSentence;
+}
+// console.log(capitalizeWords("this is a simple sentence"));
+
+/* Problem: takes a string as input and returns a new string with all whitespace characters (spaces, tabs, and newlines) removed */
+function removeWhitespace(inputString) {
+    const removedSpace = inputString.replace(/\s/g, "");
+    return removedSpace;
+}
+// console.log(removeWhitespace("  remove    whitespace  "));
+
+/* Problem: A palindrome is a word, phrase, or sequence of characters that reads the same backward as forward*/
+function isPalindrome(inputString) {
+    const cleanedString = inputString.toLowerCase().replace(/\s/g, "");
+    const reversedString = cleanedString.split("").reverse().join("");
+    return cleanedString === reversedString;
+}
+
+// console.log(isPalindrome("A man a plan a canal Panama"));

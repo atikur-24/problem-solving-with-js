82749b4928727a101ea950a302467882e9d60945
atikur-7 <asraful0508@gmail.com>
2024-03-02 23:33:30 +0600

    calculates the sum of the digits of a given positive integer

--- number.js
+++ number.js
@@ -1,25 +1,18 @@
-function publicBusFare(participants) {
-    if (typeof participants != "number" || participants <= 50) {
-        return "provide valid participants...";
-    }
-    let remaining = 0;
-    remaining = participants % 50;
-    if (remaining >= 11) {
-        remaining = remaining % 11;
-    }
-    let costOfPublicBus = remaining * 250;
-    return costOfPublicBus;
-}
-
-const costOfPublicBus = publicBusFare(51);
-// console.log(costOfPublicBus);
-
-/* A palindrome number reads the same backward as forward */
+/* Problem: A palindrome number reads the same backward as forward */
 function isPalindrome(number) {
     const strNum = String(number);
     const reversedStr = strNum.split("").reverse().join("");
     return strNum === reversedStr;
 }
 
-console.log(isPalindrome(121));
-console.log(isPalindrome(373));
+// console.log(isPalindrome(121));
+// console.log(isPalindrome(373));
+
+/* Problem: calculates the sum of the digits of a given positive integer */
+function sumOfDigits(number) {
+    const numToArr = String(number).split("");
+    const sum = numToArr.reduce((sum, digit) => sum + parseInt(digit), 0);
+    return sum;
+}
+
+console.log(sumOfDigits(723));

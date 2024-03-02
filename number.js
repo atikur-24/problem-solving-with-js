86f6900264f0c6a3d5fe56756431d17850635123
atikur-7 <asraful0508@gmail.com>
2024-03-02 23:22:36 +0600

    palindrome number reads the same backward as forward

--- number.js
+++ number.js
@@ -7,10 +7,19 @@ function publicBusFare(participants) {
     if (remaining >= 11) {
         remaining = remaining % 11;
     }
-    console.log(remaining);
     let costOfPublicBus = remaining * 250;
     return costOfPublicBus;
 }
 
 const costOfPublicBus = publicBusFare(51);
 // console.log(costOfPublicBus);
+
+/* A palindrome number reads the same backward as forward */
+function isPalindrome(number) {
+    const strNum = String(number);
+    const reversedStr = strNum.split("").reverse().join("");
+    return strNum === reversedStr;
+}
+
+console.log(isPalindrome(121));
+console.log(isPalindrome(373));

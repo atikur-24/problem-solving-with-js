aee8dd4a33d78c9255a99f8403fa5ef63c7f2c4f
atikur-7 <asraful0508@gmail.com>
2024-03-03 17:27:57 +0600

    takes a number as input and returns the equivalent words

--- number.js
+++ number.js
@@ -38,3 +38,39 @@ function fibonacciSequence(count) {
 }
 
 // console.log(fibonacciSequence(5));
+
+/* Problem: Create a function that takes a number as input and returns the equivalent words */
+function numberToWords(number) {
+    const words = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+    const teens = ["", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"];
+
+    const tens = ["", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"];
+
+    if (number === 0) return "Zero";
+    if (number >= 1000) return "Above one thousand not allow";
+
+    function convertLessThanThousand(num) {
+        if (num === 0) return "";
+        else if (num < 10) return words[num];
+        else if (num < 20) return teens[num - 10];
+        else {
+            const digit = num % 10;
+            const ten = Math.floor(num / 10);
+            return tens[ten] + (digit ? "-" + words[digit] : "");
+        }
+    }
+
+    function convert(num) {
+        if (num === 0) return "";
+        else if (num < 100) return convertLessThanThousand(num);
+        else {
+            const hundred = Math.floor(num / 100);
+            const remainder = num % 100;
+            return words[hundred] + " Hundred" + (remainder ? " and " + convertLessThanThousand(remainder) : "");
+        }
+    }
+
+    return convert(number);
+}
+
+console.log(numberToWords(9000));
